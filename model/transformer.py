9ad1f4bbc05a9c36de3188a0fc7f25a007580be8
taekkim <taekkim@student.42seoul.kr>
2022-03-08 05:00:42 +0900

    feat(poetry): updated

--- model/transformer.py
+++ model/transformer.py
@@ -1,8 +1,7 @@
 import bentoml
 from transformers import AutoTokenizer, GPT2LMHeadModel
 
-# model = AutoModelForSeq2SeqLM.from_pretrained("t5-base")
-model = GPT2LMHeadModel.from_pretrained("./../../../Downloads/test-kogpt-trained-epoch6")
+model = GPT2LMHeadModel.from_pretrained("/Users/taekkim/goinfre/models/model")
 tokenizer = AutoTokenizer.from_pretrained("skt/kogpt2-base-v2")
 
 tag = bentoml.transformers.save("math_code_generation", model=model, tokenizer=tokenizer)
